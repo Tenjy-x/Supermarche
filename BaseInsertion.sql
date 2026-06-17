3ebad9662f64d3063ba35985c9a2196f053686eb
Tenjy-x <tendrymk848@gmail.com>
2026-06-17 09:16:54 +0300

    insertion

---
+++ BaseInsertion.sql
@@ -0,0 +1,11 @@
+INSERT INTO Produits (Nom, Prix, Quantite_en_stock) VALUES
+('Biscuit', 10.00, 100),
+('Chocolat', 20.00, 50),
+('Bonbon', 15.00, 75),
+('Jus de fruit', 25.00, 30),
+('Eau minérale', 5.00, 200);
+
+INSERT INTO Caisse (numero_caisse) VALUES
+('Caisse 1'),
+('Caisse 2');
+
